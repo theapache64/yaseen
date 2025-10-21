bfd1a7d7e03fed8f259bad91d5844f8206b4ee94
theapache64 <theapache64@gmail.com>
2025-10-21 18:08:09 +0530

    Add button to jump to first page in PageFragment

--- app/src/main/java/com/github/theapache64/yaseen/MainActivity.kt
+++ app/src/main/java/com/github/theapache64/yaseen/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.github.theapache64.yaseen
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -25,11 +28,14 @@ val yaseen = arrayOf(
 
 
 class MainActivity : FragmentActivity() {
+
+    private val vpYaseen by lazy {
+        findViewById<ViewPager2>(R.id.vp_yaseen)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
-
 
         val adapter = object : FragmentStateAdapter(this) {
             override fun createFragment(position: Int): Fragment {
@@ -39,14 +45,18 @@ class MainActivity : FragmentActivity() {
             override fun getItemCount(): Int = yaseen.size
         }
 
-        findViewById<ViewPager2>(R.id.vp_yaseen).apply {
+        vpYaseen.apply {
             this.adapter = adapter
             this.layoutDirection = ViewPager2.LAYOUT_DIRECTION_RTL
         }
     }
+
+    fun goToFirstPage() {
+        vpYaseen.setCurrentItem(0, true)
+    }
 }
 
-class PageFragment : Fragment() {
+class PageFragment() : Fragment() {
     companion object {
         private const val KEY_PAGE = "page"
         fun newInstance(page: Int): PageFragment {
@@ -58,25 +68,56 @@ class PageFragment : Fragment() {
         }
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val position = arguments?.getInt(KEY_PAGE) ?: -1
-        val ivPage = ImageView(context).apply {
-            // full screen
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            scaleType = ImageView.ScaleType.FIT_XY
-            scaleX = 1.1f
+        val layout = context?.let { ctx ->
+            val ivPage = ImageView(ctx).apply {
+                // full screen
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                scaleType = ImageView.ScaleType.FIT_XY
+                scaleX = 1.1f
 
-            // image
-            setImageResource(yaseen[position])
+                // image
+                setImageResource(yaseen[position])
 
+            }
+
+            val btn = if (position == yaseen.lastIndex) {
+                Button(ctx).apply {
+                    layoutParams = FrameLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                    ).apply {
+                        gravity = Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL
+                    }
+
+                    setOnClickListener {
+                        (activity as MainActivity).goToFirstPage()
+                    }
+
+                    text = "GO TO FIRST PAGE"
+                }
+            } else {
+                null
+            }
+
+            // root
+            FrameLayout(ctx).apply {
+                addView(ivPage)
+                if (btn != null) {
+                    addView(btn)
+                }
+            }
         }
-        return ivPage
+
+        return layout
     }
 }
