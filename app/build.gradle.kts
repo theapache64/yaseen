13b99c57f3e8f73eb266ce27d36fec481bdcf820
theapache64 <theapache64@gmail.com>
2025-10-22 01:34:19 +0530

    Update app version to 25.10.22.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,9 @@ android {
         applicationId = "com.github.theapache64.yaseen"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        // [latest version - i promise!]
+        versionName = "25.10.22.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
